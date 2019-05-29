a8539965b82e760d7ae1ed491632bb5c852485d6
Yiteng <you@example.com>
2019-05-29 10:54:48 -0700

    reformatted text

--- src/App.js
+++ src/App.js
@@ -3,16 +3,16 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      <p>
+      <h3>
         The Statue of Liberty's torch heads to new museum<br/>
+      </h3> 
         <address>
           by Kate Farley (<a href="mailto:kate.farley@nytimes.com">kate.farley@nytimes.com</a>)<br/>
           <time dateTime="2018-11-22">November 22, 2018</time><br/>
         </address>
-      </p>
-      <p>
+      <h4>
         The Statue of Liberty's original torch is getting a new home.
-      </p>
+      </h4>
       <p>
         Made of copper and gold so that it would shine bright, the first torch took a beating from the weather and was replaced<br/>
         by a replica in <time dateTime="1984">1984</time>. It's been stored in the statue's pedestal ever since.
@@ -26,9 +26,9 @@ function App() {
         <q lang="en">Although it is not one of the most difficult things we have ever moved,</q> says Douglas Phelps, who oversaw the<br/>
         relocation of the torch, <q lang="en">it is certainly the most important.</q>
       </p>
-      <p>
+      <h4>
         The history of the Statue of Liberty
-      </p>
+      </h4>
       <p>
         Not only is the Statue of Liberty the symbol of New York City, she is also a <abbr title="United Nations Educational, Scientific and Cultural Organization">UNESCO</abbr> World Heritage site under the<br/>
         designation of <em>Outstanding Universal Value</em>.
@@ -63,22 +63,14 @@ function App() {
         The inspiration gallery, which is where the torch will live, will have floor-to-ceiling glass windows looking out at Lower<br/>
         Manhattan, including the World Trade Center.
       </p>
-      <p>
+      <h4>
         Some facts about the Statue of Liberty
-      </p>
+      </h4>
       <ul>
-        <p>
-          <li>Total overall height from the base of the pedestal foundation to the tip of the torch is <strong>305 feet, 6 inches</strong></li>
-        </p>
-        <p>
-          <li>Height of the Statue from her heel to the top of her head is <strong>111 feet, 6 inches</strong></li>
-        </p>
-        <p>
-          <li>The Statue has a <strong>35-foot</strong> waistline</li>
-        </p>
-        <p>
-          <li>Total weight of the Statue of Liberty is <strong>225 tons</strong> (or <strong>450,000 pounds</strong>)</li>
-        </p>
+        <li><p>Total overall height from the base of the pedestal foundation to the tip of the torch is <strong>305 feet, 6 inches</strong></p></li>
+        <li><p>Height of the Statue from her heel to the top of her head is <strong>111 feet, 6 inches</strong></p></li>
+        <li><p>The Statue has a <strong>35-foot</strong> waistline</p></li>
+        <li><p>Total weight of the Statue of Liberty is <strong>225 tons</strong> (or <strong>450,000 pounds</strong>)</p></li>
       </ul>
     </div>
   );
