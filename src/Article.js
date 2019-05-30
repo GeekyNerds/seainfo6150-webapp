105df9e03efd1242767822b5554bd562a4423280
Yiteng <you@example.com>
2019-05-29 17:57:22 -0700

    added Article component

--- src/Article.js
+++ src/Article.js
@@ -2,9 +2,7 @@ import React from 'react';
 
 const Article = () => (
   <div>
-      <h2>
-          You can take a pie on a plane<br/>
-      </h2>
+      <h2>You can take a pie on a plane<br/></h2>
       <address>By: Cathryn Lindsey<br/></address>
       <time dateTime="2018-11-21 22:57:55">Wed, 21 Nov 2018 22:57:55 GMT</time><br/>
       <p>
