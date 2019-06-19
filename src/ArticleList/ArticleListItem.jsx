5c0edec51056285a845644dc0731330281a071b1
Yiteng <you@example.com>
2019-06-19 13:56:30 -0700

    updated CSS

--- src/ArticleList/ArticleListItem.jsx
+++ src/ArticleList/ArticleListItem.jsx
@@ -4,13 +4,15 @@ import ArticleImage from './ArticleImage'
 
 const ArticleListItem = ({ title, date, year, author, shortText, url }) => (
     <li className={styles.item}>
-        
-        <article>
         <ArticleImage url={url} title={title} />
+        <article className={styles.article}>
+        
             <h2 className={styles.title}>{title}<br/></h2>
+            
             <p className={styles.paragraph}>
                 {shortText}
             </p>
+            
             <section className={styles.section}>
                 <address>By: {author}<br/></address>
                 <time className={styles.time} dateTime={year}>{date}</time><br/>
